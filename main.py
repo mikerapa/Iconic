bc82bcbb235485985a8fd99929f9bf240f4982ac
Mike Rapa <mike@mikerapa.com>
2025-01-05 16:49:14 -0500

    Added the main view of the application and a basic path entry

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from textual.app import App
+from ui.main_view import MainView
 from model.log_handler import get_logger
 
 logger = get_logger(__name__)
 
+class IconicApp(App):
+    """Main Iconic application."""
+    
+    def compose(self):
+        """Create child widgets for the app."""
+        yield MainView()
+
 def main():
     logger.debug("Iconic started")
-    print("Hello from iconic!")
+    app = IconicApp()
+    app.run()
 
 if __name__ == "__main__":
+    # TODO: add command argument for log level
     main()
